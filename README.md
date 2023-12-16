19ea444418ccd2d468cc65de5c06a024f2eab408
Chorn Sokun <csokun@users.noreply.github.com>
2023-12-16 18:38:23 +1100

    Lazy loading (#1)
    * container
    * screenshot

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Phoenix Liveview + React 18 + Antd
 
+![Lazy loading component](./preview.png)
+
 To start your Phoenix server:
 
 - Run `mix setup` to install and setup dependencies
 - Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
 
 Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+## Container
+
+```bash
+./container.sh
+# visit http://localhost:4000
+```
 
 ## References
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -37,13 +37,13 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :lv_react, LvReactWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: "http"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
